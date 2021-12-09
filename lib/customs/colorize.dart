874ed89eaeeb0174c7bbd23673033d01c2ac5212
taufiqjack <taufiqdwic@gmail.com>
2021-12-09 16:44:00 +0700

    adding custom colors

---
+++ lib/customs/colorize.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class Colorize {
+  static const greenOne = Color(0xFF1CAF60);
+  static const greyOne = Color(0xFFE8F7EF);
+  static const greenTwo = Color(0xFF60C78F);
+  static const greyTwo = Color(0xFFA4A4A4);
+  static const greenThree = Color(0xFFA4DFBF);
+  static const greeFour = Color(0xFF32B770);
+  static const greenFive = Color(0xFFBAE7CF);
+  static const yellowOne = Color(0xFFFFC21C);
+  static const greyThree = Color(0xFFE7E7E7);
+  static const purpleOne = Color(0xFFB3CAFF);
+  static const yellowTwo = Color(0xFFFFE49A);
+  static const greyFour = Color(0xFFC8C8C8);
+}

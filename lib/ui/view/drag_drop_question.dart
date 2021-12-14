9f80b4bcde6a0dd38486b1d023c52d0a9380ad8d
taufiqjack <taufiqdwic@gmail.com>
2021-12-14 16:36:16 +0700

    update bug

--- lib/ui/view/drag_drop_question.dart
+++ lib/ui/view/drag_drop_question.dart
@@ -1,8 +1,7 @@
-import 'dart:io';
-
 import 'package:dotted_border/dotted_border.dart';
 import 'package:e_learning_uji/data/data.dart';
 import 'package:e_learning_uji/data/string.dart';
+import 'package:e_learning_uji/data/stringlist.dart';
 import 'package:e_learning_uji/ui/view/index_view.dart';
 import 'package:e_learning_uji/ui/widget/draggable_widget.dart';
 import 'package:flutter/material.dart';
@@ -79,7 +78,7 @@ class _DragDropViewState extends State<DragDropView> {
                   ),
                 ),
                 Container(
-                  height: 500,
+                  height: 600,
                   width: 400,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
@@ -109,36 +108,36 @@ class _DragDropViewState extends State<DragDropView> {
                               _merpati.add(data);
                             }),
                           ),
-                          target(
-                            context,
-                            text: PIPIT,
-                            birds: _pipit,
-                            birdsName: [BirdsName.merpati],
-                            onAccept: (data) => setState(() {
-                              removeAll(data);
-                              _pipit.add(data);
-                            }),
-                          ),
-                          target(
-                            context,
-                            text: PERKUTUT,
-                            birds: _perkutut,
-                            birdsName: [BirdsName.merpati],
-                            onAccept: (data) => setState(() {
-                              removeAll(data);
-                              _perkutut.add(data);
-                            }),
-                          ),
-                          target(
-                            context,
-                            text: CENDERAWASIH,
-                            birds: _cenderawasih,
-                            birdsName: [BirdsName.merpati],
-                            onAccept: (data) => setState(() {
-                              removeAll(data);
-                              _cenderawasih.add(data);
-                            }),
-                          ),
+                          // target(
+                          //   context,
+                          //   text: PIPIT,
+                          //   birds: _pipit,
+                          //   birdsName: [BirdsName.merpati],
+                          //   onAccept: (data) => setState(() {
+                          //     removeAll(data);
+                          //     _pipit.add(data);
+                          //   }),
+                          // ),
+                          // target(
+                          //   context,
+                          //   text: PERKUTUT,
+                          //   birds: _perkutut,
+                          //   birdsName: [BirdsName.merpati],
+                          //   onAccept: (data) => setState(() {
+                          //     removeAll(data);
+                          //     _perkutut.add(data);
+                          //   }),
+                          // ),
+                          // target(
+                          //   context,
+                          //   text: CENDERAWASIH,
+                          //   birds: _cenderawasih,
+                          //   birdsName: [BirdsName.merpati],
+                          //   onAccept: (data) => setState(() {
+                          //     removeAll(data);
+                          //     _cenderawasih.add(data);
+                          //   }),
+                          // ),
                         ],
                       ),
                     ],
@@ -176,7 +175,7 @@ class _DragDropViewState extends State<DragDropView> {
           children: [
             ...birds
                 .map((bird) => Padding(
-                    padding: const EdgeInsets.only(top: 25),
+                    padding: const EdgeInsets.all(25),
                     child: SizedBox(
                         height: 70,
                         width: 100,
@@ -193,43 +192,126 @@ class _DragDropViewState extends State<DragDropView> {
 
   Widget target(
     BuildContext context, {
+    // List<ListName>? listName,
     String? text,
     required List<Birds> birds,
     required List<BirdsName> birdsName,
     required DragTargetAccept<Birds> onAccept,
   }) =>
       Padding(
-        padding: const EdgeInsetsDirectional.only(top: 20),
-        child: DottedBorder(
-          borderType: BorderType.RRect,
-          strokeWidth: 2,
-          radius: const Radius.circular(5),
-          color: Colors.grey.shade400,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: Colors.grey.shade500,
-            ),
-            height: 100,
+        padding: const EdgeInsetsDirectional.only(top: 10),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: 550,
             width: 100,
-            child: DragTarget<Birds>(
-              builder: (context, cd, rj) => Stack(
+            child: Stack(children: [
+              Column(
                 children: [
-                  Center(
-                    child: buildText(text!),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: SizedBox(
+                      height: 80,
+                      width: 100,
+                      child: DottedBorder(
+                        borderType: BorderType.RRect,
+                        strokeWidth: 2,
+                        radius: const Radius.circular(5),
+                        color: Colors.grey.shade400,
+                        child: const Center(
+                          child: Text(
+                            'Merpati',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  ...birds
-                      .map((bird) => Padding(
-                          padding: const EdgeInsets.only(top: 25),
-                          child: DraggableWidget(bird: bird)))
-                      .toList(),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: SizedBox(
+                      height: 80,
+                      width: 100,
+                      child: DottedBorder(
+                          borderType: BorderType.RRect,
+                          strokeWidth: 2,
+                          radius: const Radius.circular(5),
+                          color: Colors.grey.shade400,
+                          child: const Center(
+                            child: Text(
+                              'Perkutut',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          )),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: SizedBox(
+                        height: 80,
+                        width: 100,
+                        child: DottedBorder(
+                            borderType: BorderType.RRect,
+                            strokeWidth: 2,
+                            radius: const Radius.circular(5),
+                            color: Colors.grey.shade400,
+                            child: const Center(
+                              child: Text(
+                                'Pipit',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ))),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: SizedBox(
+                      height: 80,
+                      width: 100,
+                      child: DottedBorder(
+                        borderType: BorderType.RRect,
+                        strokeWidth: 2,
+                        radius: const Radius.circular(5),
+                        color: Colors.grey.shade400,
+                        child: const Center(
+                          child: Text(
+                            'Cenderawasih',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
-              onWillAccept: (data) => true,
-              onAccept: (data) {
-                onAccept(data);
-              },
-            ),
+              DragTarget<Birds>(
+                builder: (context, cd, rj) => Column(
+                  children: [
+                    // Center(
+                    //   child: buildText(text!),
+                    // ),
+                    buildText(text!),
+
+                    ...birds
+                        .map((bird) => Padding(
+                            padding: const EdgeInsetsDirectional.only(top: 10),
+                            child: DraggableWidget(bird: bird)))
+                        .toList(),
+                  ],
+                ),
+                onWillAccept: (data) => true,
+                onAccept: (data) {
+                  onAccept(data);
+                },
+              ),
+            ]),
           ),
         ),
       );
@@ -241,7 +323,7 @@ class _DragDropViewState extends State<DragDropView> {
   Widget buildText(String text) => Text(
         text,
         style: const TextStyle(
-          color: Colors.white,
+          color: Colors.transparent,
           fontSize: 14,
           fontWeight: FontWeight.bold,
         ),
